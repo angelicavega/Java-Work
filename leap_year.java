6a9d646125905c57df0b81da6bda76a6dca0db94
angelicavegaaponte <angelicavegaaponte@users.noreply.github.com>
2020-09-16 23:20:03 -0400

    leap leap

---
+++ leap_year.java
@@ -0,0 +1,29 @@
+public class leap_year {
+
+    public static void main(String[] args) {
+
+        int year = 2012;
+        boolean leap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                // leap year is divisible by 400
+                if ( year % 400 == 0)
+                    leap = true;
+                else
+                    leap = false;
+            }
+            else
+                leap = true;
+        }
+        else
+            leap = false;
+
+        if(leap)
+            System.out.println(year + " is a leap year.");
+        else
+            System.out.println(year + " is not a leap year.");
+    }
+}
